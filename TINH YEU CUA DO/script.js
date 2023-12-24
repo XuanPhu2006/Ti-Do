de87e0dcdc44f614e23763b3e193532ff6afe1be
XuanPhu2006 <111589653+XuanPhu2006@users.noreply.github.com>
2023-12-24 14:30:39 +0700

    Add files via upload

--- TINH YEU CUA DO/script.js
+++ TINH YEU CUA DO/script.js
@@ -1,18 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Ngày bắt đầu mối quan hệ
-    const startDate = new Date('2022/06/05'); // Thay YYYY, MM, DD bằng ngày bắt đầu của bạn
+const yourDate = new Date("2022-06-05"),
+music = ['ido', 'noinaycoanh', 'nguoiamphu'];
 
-    function updateCountdown() {
-        const currentDate = new Date();
-        const timeDiff = currentDate - startDate;
+document.addEventListener('DOMContentLoaded', function(){
+      var rootTime = document.querySelector("time");
 
-        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+      document.querySelector("anni").textContent = `${(yourDate.getDate()>9)?yourDate.getDate():"0"+yourDate.getDate()}-${(yourDate.getMonth()>8)?(yourDate.getMonth()+1):"0"+(yourDate.getMonth()+1)}-${yourDate.getFullYear()}`;
+      
+      document.querySelector("date").textContent = Math.floor( Math.floor((new Date() - yourDate) / 1000) / 60 / 60 / 24)+" DAYS";
 
-        document.getElementById('countdown').innerHTML = `${days} ngày ${hours} giờ ${minutes} phút ${seconds} giây`;
-    }
+      function olock() {
+            var today = new Date(),
+            hrs = (Math.floor( Math.floor((today - yourDate) / 1000) / 60 / 60)) % 24,
+            min = (Math.floor( Math.floor((today - yourDate) / 1000) / 60)) % 60,
+            sec =  Math.floor((today - yourDate) / 1000) % 60;
+            rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
+      } olock();
+      var timer = setInterval(function(){olock()}, 1000);
+      document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
 
-    setInterval(updateCountdown, 1000);
-});
+      document.getElementsByTagName("body")[0].insertAdjacentHTML(
+            "beforeend",
+            "<div id='mask'></div>"
+      );
+
+}, false);
